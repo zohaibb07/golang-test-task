930190ac1f8254ce5fcfd2b78578d9b4b44bffb1
zohaibb07 <zohaib.dev07@gmail.com>
2022-01-12 09:18:29 +0500

    [Task-1] - main.go & go.mod files added

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"golang-test-task/task_one"
+)
+
+func main() {
+
+	sample_str := "23-okaaa-48-caba-56-haha"
+
+	fmt.Println(task_one.TestValidity(sample_str))
+
+}
